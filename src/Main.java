4c5cffb3bbd7b65d5d95145a399517e0bf35e984
Miguel <mil.ferreira@fct.unl.pt>
2022-09-17 12:14:46 +0100

    Updated main class with Dog and Cat objects.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Super animal git demo.");
+        Animal dog = new Dog("Bobby");
+        Animal cat = new Cat("Kitty");
+        System.out.println(dog.getName() + " says " + dog.speak());
+        System.out.println(cat.getName() + " says " + cat.speak());
     }
 }
